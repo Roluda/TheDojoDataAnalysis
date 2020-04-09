f1849445e403e8f76dbd6212ba0a01b2ddcefcb3
Robin Daraban <robin.daraban@gmx.de>
2020-04-09 12:58:32 +0200

    Add Curve DiagramFrame and Event Diagram Frame

--- DiagramFrames.py
+++ DiagramFrames.py
@@ -1,11 +1,13 @@
 import matplotlib.pyplot as mpl
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
 from matplotlib.figure import Figure
+import dateutil
+import datetime
 import numpy as np
 import tkinter as tk
 import Controller as C
 
-diagrams = {"Bar Diagram","Scatter Diagram","Pie Diagram"}
+diagrams = {"Bar Diagram","Scatter Diagram","Pie Diagram", "Event Diagram", "Curve Diagram"}
 
 class EmptyDiagramFrame(tk.Frame):
     def __init__(self, master=None, controller=None, cnf={}, **kw):
@@ -31,13 +33,15 @@ class EmptyDiagramFrame(tk.Frame):
         self.refreshButton.grid(row=0, column = 1, sticky = "E")
         if self.newDiagramTkVar.get() == "Bar Diagram":
             self.diagramFrame = BarDiagramFrame(self, controller = self.controller)
-            self.onRefresh.append(self.diagramFrame.refresh)
         elif self.newDiagramTkVar.get() == "Scatter Diagram":
             self.diagramFrame = ScatterDiagramFrame(self, controller = self.controller)
-            self.onRefresh.append(self.diagramFrame.refresh)
         elif self.newDiagramTkVar.get() == "Pie Diagram":
             self.diagramFrame = PieDiagramFrame(self, controller=self.controller)
-            self.onRefresh.append(self.diagramFrame.refresh)
+        elif self.newDiagramTkVar.get() == "Event Diagram":
+            self.diagramFrame = EventDiagramFrame(self, controller=self.controller)
+        elif self.newDiagramTkVar.get() == "Curve Diagram":
+            self.diagramFrame = CurveDiagramFrame(self, controller=self.controller)
+        self.onRefresh.append(self.diagramFrame.refresh)
         self.diagramFrame.grid(sticky="SE")
 
     def refresh(self):
@@ -217,3 +221,126 @@ class PieDiagramFrame(tk.Frame):
             print(type(e))
             print(e.args)
             print(e)
+
+
+class EventDiagramFrame(tk.Frame):
+    def __init__(self, master=None, controller = None, cnf={}, **kw):
+        super().__init__(master=master, cnf=cnf, **kw)
+        self.controller = controller
+        self.timeAttributeSet = sorted(C.uniqueTimeAttributesInData(self.controller.treeOutput()))
+        self.nameAttributeSet = sorted(C.uniqueLanguageAttributesInData(self.controller.treeOutput()))
+
+        self.settingParent = tk.Frame(self)
+
+        self.timeAttributeTkVar = tk.StringVar(self)
+        self.timeAttributeTkVar.set("select time attribut")
+        self.timeAttributeTkVar.trace('w', self.refresh)
+        self.timeAttributeSelect = tk.OptionMenu(self.settingParent, self.timeAttributeTkVar, *self.timeAttributeSet)
+        self.timeAttributeSelect.grid(row = 0, column =0, sticky="S")
+
+
+        self.nameAttributeTkVar = tk.StringVar(self)
+        self.nameAttributeTkVar.set("select event attribut")
+        self.nameAttributeTkVar.trace('w', self.refresh)
+        self.nameAttributeSelect = tk.OptionMenu(self.settingParent, self.nameAttributeTkVar, *self.nameAttributeSet)
+        self.nameAttributeSelect.grid(row = 0, column =1, sticky="W")
+        self.settingParent.grid(row=2, column=0, sticky="S")
+
+        self.fig, self.ax = mpl.subplots()
+        self.updateFigure()
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().grid(row=0, sticky="NE")
+        self.toolbarFrame = tk.Frame(self)
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self.toolbarFrame)
+        self.toolbar.update()
+        self.toolbarFrame.grid()
+        self.canvas.get_tk_widget().grid(row=1, sticky="NE")
+
+    def refresh(self, *args):
+        self.ax.clear()
+        self.updateFigure()
+        self.canvas.draw()
+
+    def updateFigure(self):
+        try:
+            pointsX=[]
+            pointsY=[]
+            names = []
+            for element in self.controller.treeOutput():
+                flat = C.flatElement(element)
+                time = dateutil.parser.parse(flat[self.timeAttributeTkVar.get()])
+                if flat[self.nameAttributeTkVar.get()] not in names:
+                    names.append(flat[self.nameAttributeTkVar.get()])
+                pointsX.append(time)
+                pointsY.append(names.index(flat[self.nameAttributeTkVar.get()]))
+            self.ax.plot(pointsX, pointsY, '|')
+            self.ax.set_xlabel(self.timeAttributeTkVar.get())
+            self.ax.set_yticks(np.arange(len(names)))
+            self.ax.set_yticklabels(names, ha ="right")
+            self.ax.set_title(self.controller.root.tag)
+            self.fig.tight_layout()
+
+        except Exception as e:
+            pass
+
+
+class CurveDiagramFrame(tk.Frame):
+    def __init__(self, master=None, controller = None, cnf={}, **kw):
+        super().__init__(master=master, cnf=cnf, **kw)
+        self.controller = controller
+        self.timeAttributeSet = sorted(C.uniqueTimeAttributesInData(self.controller.treeOutput()))
+        self.numAttributeSet = sorted(C.uniqueNumAttributesInData(self.controller.treeOutput()))
+
+        self.settingParent = tk.Frame(self)
+
+        self.timeAttributeTkVar = tk.StringVar(self)
+        self.timeAttributeTkVar.set("select time attribut")
+        self.timeAttributeTkVar.trace('w', self.refresh)
+        self.timeAttributeSelect = tk.OptionMenu(self.settingParent, self.timeAttributeTkVar, *self.timeAttributeSet)
+        self.timeAttributeSelect.grid(row = 0, column =0, sticky="S")
+
+
+        self.numAttributeTkVar = tk.StringVar(self)
+        self.numAttributeTkVar.set("select value attribut")
+        self.numAttributeTkVar.trace('w', self.refresh)
+        self.numAttributeSelect = tk.OptionMenu(self.settingParent, self.numAttributeTkVar, *self.numAttributeSet)
+        self.numAttributeSelect.grid(row = 0, column =1, sticky="W")
+        self.settingParent.grid(row=2, column=0, sticky="S")
+
+        self.fig, self.ax = mpl.subplots()
+        self.updateFigure()
+
+        self.canvas = FigureCanvasTkAgg(self.fig, master=self)
+        self.canvas.draw()
+        self.canvas.get_tk_widget().grid(row=0, sticky="NE")
+        self.toolbarFrame = tk.Frame(self)
+        self.toolbar = NavigationToolbar2Tk(self.canvas, self.toolbarFrame)
+        self.toolbar.update()
+        self.toolbarFrame.grid()
+        self.canvas.get_tk_widget().grid(row=1, sticky="NE")
+
+    def refresh(self, *args):
+        self.ax.clear()
+        self.updateFigure()
+        self.canvas.draw()
+
+    def updateFigure(self):
+        try:
+            pointsX=[]
+            pointsY=[]
+            names = []
+            for element in self.controller.treeOutput():
+                flat = C.flatElement(element)
+                time = dateutil.parser.parse(flat[self.timeAttributeTkVar.get()])
+                pointsX.append(time)
+                pointsY.append(float(flat[self.numAttributeTkVar.get()]))
+            self.ax.plot(pointsX, pointsY)
+            self.ax.set_xlabel(self.timeAttributeTkVar.get())
+            self.ax.set_ylabel(self.numAttributeTkVar.get())
+            self.ax.set_title(self.controller.root.tag)
+            self.fig.tight_layout()
+
+        except Exception as e:
+            pass
